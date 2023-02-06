bacf0522863d6b86df079c5bbf78b2e75ad99066
JontePonte <john_nordstrand@hotmail.com>
2023-02-06 18:53:55 +0100

    Made the data look a lot better

--- test1.py
+++ test1.py
@@ -4,4 +4,15 @@ import pandas as pd
 
 df = pd.read_excel('data/n_fot2022-01-12.xls')
 
+# Clean the data
+df.dropna(inplace=True)         # Remove None and NaN
+df = df.set_index('Unnamed: 0') # Set date and time to index
+df.columns = df.loc['Tid']      # Set correct labels on columns
+df = df[1:]                     # Remove first row
+df.columns.name = ''            # Remove ugly label on columns
+df.index.name = 'Tid'           # Remove ugly "Unknown: 0"
+
+# Make total consumption from negative to positive
+df['Total förbrukning'] = df['Total förbrukning'] * -1
+
 print(df)
